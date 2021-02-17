6ab589c8619ba179e49fe54ebee91aa26da022b3
Nipun David <nipun18david@gmail.com>
2021-02-17 21:46:44 +0530

    connection successfully established

---
+++ server.js
@@ -0,0 +1,40 @@
+const PROTO_PATH = __dirname + "/proto/employee.proto";
+
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
+const _ = require("lodash");
+
+let packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+let employee_proto = grpc.loadPackageDefinition(packageDefinition).employee;
+
+let { employees } = require("./data.js");
+
+function main() {
+  let server = new grpc.Server();
+  server.addService(employee_proto.Employee.service, {
+    getDetails: getDetails,
+  });
+  server.bind("0.0.0.0:4500", grpc.ServerCredentials.createInsecure());
+  server.start();
+}
+
+function getDetails(call, callback) {
+  callback(null, {
+    // message: _.find(employees, { id: call.request.id }),
+    message: {
+      id: 1,
+      email: "xyzd@abcd.com",
+      firstName: "First1",
+      lastName: "Last1",
+    },
+  });
+}
+
+main();
